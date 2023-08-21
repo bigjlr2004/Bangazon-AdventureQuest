7b60008ecf0d633a0b18439be76f53728275aed6
John Roberts <bigjlr2004@yahoo.com>
2023-08-21 09:29:08 -0500

    Phase 4 completed

---
+++ Robe.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+
+namespace Quest
+{
+    // The class should have the following mutable properties.
+    // Colors - a list of strings to hold the colors of the robe
+    // Length - an integer describing the length of the robe in inches
+    // The class should not contain any methods or constructors.
+    public class Robe
+    {
+        public List<string> Colors { get; set; }
+
+        public int Length { get; set; }
+    }
+}
+
+
+
+
